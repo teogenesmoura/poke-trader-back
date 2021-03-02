a64013bab52ad96f12f2972953bcb9d3b0a402a2
Teogenes Moura <mourateogenes@gmail.com>
2021-03-02 15:56:47 -0300

    US04-CreateEntryInHistory (#10)
    * #8 Creating basic structure for Entry and moving encode_auth_token and decode_auth_token to outside the User class
    
    * #8 Introducing entry post endpoint
    
    * #8 Removing SingleEntryAPI
    
    * #8 Moving Entry APIs to entry file

--- app/entry/controllers.py
+++ app/entry/controllers.py
@@ -8,11 +8,11 @@ from app.util.util import build_response_object, get_user_from_token
 from app.messages import RESOURCE_CREATION_SUCCESS, USER_NOT_EXIST, INVALID_USER_ID, NO_DATA_FOUND, \
                          INCOMPLETE_DATA, INVALID_TOKEN, GENERIC_ERROR
 from app.api_paths import ENTRY_BASE_URL
-entry_blueprint = Blueprint('entry', __name__, url_prefix=ENTRY_BASE_URL)
+entry_blueprint = Blueprint('entry', __name__)
 
 class SingleEntryAPI(MethodView):
     """ Handles requests for a single Entry object """
-    def post(self):
+    def post(self, history_id):
         #checa se usuário tem token válido
         auth_header = request.headers.get('Authorization')
         post_data = request.get_json()
@@ -51,11 +51,43 @@ class SingleEntryAPI(MethodView):
         response = build_response_object('success', entry_info, "")
         return make_response(jsonify(response)), 200
 
+class EntriesAPI(MethodView):
+    """ Returns all Entry objects associated with a History object """
+    def get(self, history_id):
+        #checa se usuário tem token válido
+        auth_header = request.headers.get('Authorization')
+        post_data = request.get_json()
+        user, user_id = get_user_from_token(auth_header)
+        #checa se usuário existe
+        if(not(isinstance(user, User))):
+            response = build_response_object('fail',USER_NOT_EXIST,"")
+            return make_response(jsonify(response)), 404
+        #recupera histórico para aquele usuário
+        history = get_history_for_user(user)
+        #se histórico nao existe, retorna erro
+        if(not(isinstance(history, History))):
+            response = build_response_object('fail', HISTORY_NOT_EXIST, "")
+            return make_response(jsonify(response)), 404
+        #recupera entries para o histórico
+        try:
+            entries = Entry.query.filter_by(history_id=history_id).all()
+        except Exception as error:
+            print(error)
+            return "deu erro"
+        response = build_response_object('success', entries, "")
+        return make_response(jsonify(response)), 200
+
 entry_controller = SingleEntryAPI.as_view('entry')
+entries_controller = EntriesAPI.as_view('entries')
 
 #rules for API endpoints
 entry_blueprint.add_url_rule(
-    '/',
+    '/history/<history_id>/entry',
     view_func=entry_controller,
     methods=['POST']
+)
+entry_blueprint.add_url_rule(
+    "/history/<history_id>/entries",
+    view_func=entries_controller,
+    methods=['GET']
 )
--- app/history/controllers.py
+++ app/history/controllers.py
@@ -58,77 +58,7 @@ class HistoryAPI(MethodView):
             return make_response(jsonify(resp)), 500
         return jsonify(history)
 
-class SingleEntryAPI(MethodView):
-    """ Handles requests for a single Entry object """
-    def post(self):
-        #checa se usuário tem token válido
-        auth_header = request.headers.get('Authorization')
-        post_data = request.get_json()
-        user, user_id = get_user_from_token(auth_header)
-        #checa se usuário existe
-        if(not(isinstance(user, User))):
-            response = build_response_object('fail',USER_NOT_EXIST,"")
-            return make_response(jsonify(response)), 404
-        #recupera histórico para aquele usuário
-        history = get_history_for_user(user)
-        #se histórico nao existe, retorna erro
-        if(not(isinstance(history, History))):
-            response = build_response_object('fail', HISTORY_NOT_EXIST, "")
-            return make_response(jsonify(response)), 404
-        #insere entry no histórico
-        host = json.dumps(post_data.get('host'))
-        opponent = json.dumps(post_data.get('opponent'))
-        history_id = post_data.get('history_id')
-        if host and opponent:
-            try:
-                entry = Entry(
-                    host=host,
-                    opponent=opponent,
-                    history_id=history_id
-                )
-                db.session.add(entry)
-                db.session.commit()
-                entry_info = {
-                    'host': entry.host,
-                    'opponent': entry.opponent,
-                    'history_id': entry.history_id
-                }
-            except Exception as error:
-                response = build_response_object('fail', GENERIC_ERROR, "")
-                return make_response(jsonify(response)), 500
-        response = build_response_object('success', entry_info, "")
-        return make_response(jsonify(response)), 200
-
-entry_controller = SingleEntryAPI.as_view('entry')
-
-class EntriesAPI(MethodView):
-    """ Returns all Entry objects associated with a History object """
-    def get(self, history_id):
-        #checa se usuário tem token válido
-        auth_header = request.headers.get('Authorization')
-        post_data = request.get_json()
-        user, user_id = get_user_from_token(auth_header)
-        #checa se usuário existe
-        if(not(isinstance(user, User))):
-            response = build_response_object('fail',USER_NOT_EXIST,"")
-            return make_response(jsonify(response)), 404
-        #recupera histórico para aquele usuário
-        history = get_history_for_user(user)
-        #se histórico nao existe, retorna erro
-        if(not(isinstance(history, History))):
-            response = build_response_object('fail', HISTORY_NOT_EXIST, "")
-            return make_response(jsonify(response)), 404
-        #recupera entries para o histórico
-        try:
-            entries = Entry.query.filter_by(history_id=history_id).all()
-        except Exception as error:
-            print(error)
-            return "deu erro"
-        response = build_response_object('success', entries, "")
-        return make_response(jsonify(response)), 200
-
 history_controller = HistoryAPI.as_view('history')
-history_entries_controller = EntriesAPI.as_view('entries')
 
 #rules for API endpoints
 history_blueprint.add_url_rule(
@@ -139,10 +69,5 @@ history_blueprint.add_url_rule(
 history_blueprint.add_url_rule(
     "/<history_id>",
     view_func=history_controller,
-    methods=['GET']
-)
-history_blueprint.add_url_rule(
-    "/<history_id>/entries",
-    view_func=history_entries_controller,
     methods=['GET']
 )
